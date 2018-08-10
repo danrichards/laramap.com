f37feb12cab8a1f4e8932f36771555f7b32445c8
Florian Wartner <florian@mapawa.de>
2018-08-10 12:14:11 +0200

    Changes

--- config/deploy.php
+++ config/deploy.php
@@ -51,8 +51,7 @@ return [
 
         // Code and composer vendors are ready but nothing is built.
         'build' => [
-            'npm:install',
-            'npm:production',
+           //
         ],
 
         // Deployment is done but not live yet (before symlink)
