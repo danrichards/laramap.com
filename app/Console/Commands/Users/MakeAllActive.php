54e6c436e96ce9c7ce96e1f413d613f31499769b
Florian Wartner <florian@mapawa.de>
2018-09-05 10:25:45 +0200

    Added new command

---
+++ app/Console/Commands/Users/MakeAllActive.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands\Users;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class MakeAllActive extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:make-all-active';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Makes all users active';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $users = User::all();
+
+        foreach ($users as $user) {
+            $user->update([
+                'is_active' => true
+            ]);
+        }
+    }
+}
