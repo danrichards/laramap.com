24d08a1143aaa6b22757cfbf29476b0b12e80ba4
Laravel Shift <shift@laravelshift.com>
2018-09-06 10:28:31 +0000

    Shift bindings
    Since Laravel 5.1 requires PHP 5.5.9+ using the *new* static `class`
    property to get the fully qualified name of a class is preferred
    over using class name strings.

--- app/Nova/Resources.php
+++ app/Nova/Resources.php
@@ -18,7 +18,7 @@ class Resources extends Resource
      *
      * @var string
      */
-    public static $model = 'App\Models\Resource';
+    public static $model = \App\Models\Resource::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
