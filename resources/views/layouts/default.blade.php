f05e0474fcd46c19be8fe3dd689667c06c94f33a
Florian Wartner <fwartner@users.noreply.github.com>
2018-10-30 08:25:31 +0100

    Update default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -65,6 +65,21 @@
 
     gtag('config', 'UA-63437005-1');
 </script>
+
+<script>
+    (function(d, h, m){
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+            position: "left",          
+            widget: "0c5dyvtcey04wsg0-lf12em9zk0pl62nr-gpv6dzqvdpkqazof"                
+        })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
+    }(document, "script", "dhm"))
+</script>
 @stack('footer_scripts')
 
 </body>
